f9c70710560769efcf9ea309f6df68434c55a0c3
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-08 17:02:30 -0800

    rebuild

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/v1/books', (req, res) => {
+  client.query(`SELECT book_id, author, title, isbn, image_url FROM books;`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+
+app.post('/api/v1/books', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
+
+  client.query(`
+      INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING`,
+    [title, author, isbn, image_url, description]
+  )
+    .then(res.sendStatus(201))
+    .catch(console.error);
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
