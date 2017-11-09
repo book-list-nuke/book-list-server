e4202d560585013fc484bef2a493c0f86c00235b
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-08 18:35:19 -0800

    fdsfsd

--- server.js
+++ server.js
@@ -1,70 +1,48 @@
-'use strict';
+'use strict'
 
-// Application Dependencies
+//These are our dependencies
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
-const fs = require('fs');
+const bodyParser = require('body-parser');
 
-//Application Setup
+//Setting up our applications
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-// Database Setup
+//Setting up our database
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
-// Application Middleware
+//Instantiating middleware
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// API endpoints
-// TODO: REMOVE THIS TEST ROUTE LATER. Auto deploy test
-app.get('/test', (req, res) => res.send('hello hi!'));
+//Endpoints below here
+//Test route
+app.get('/test', (req, res) => res.send('hello world'))
 
-// Endpoint to retreive an array of book objects from database
-// TODO: ADD A $.GET TO BOOKS.JS TO LISTEN FOR THIS SEND(RESULTS.ROWS)
+//Getting
 app.get('/api/v1/books', (req, res) => {
-  client.query(`
-    SELECT book_id, title, author, image_url, isbn
-    FROM books;`)
+  client.query(`SELECT * FROM books;`)
     .then(results => res.send(results.rows))
-    .catch(console.error)
+    .catch(console.error);
 });
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
-loadDb();
+app.post('/api/v1/books', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
 
-// Starting a UNIX-Socket for connections on this port
-app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
-
-function loadBooks() {
-  fs.readFile('data/books.json', (err, fd) => {
-    JSON.parse(fd.toString()).forEach(ele => {
-      client.query(
-        `INSERT INTO books
-        (title, author, isbn, image_url, description)
-        VALUES($1, $2, $3, $4, $5)
-        ON CONFLICT (isbn) DO NOTHING;`,
-        [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
-      )
-        .catch(console.error);
-    })
-  });
-}
-
-function loadDb() {
   client.query(`
-    CREATE TABLE IF NOT EXISTS books(
-      book_id SERIAL PRIMARY KEY,
-      title VARCHAR(255) NOT NULL,
-      author VARCHAR(255) NOT NULL,
-      isbn VARCHAR(21) NOT NULL,
-      image_url VARCHAR(255) NOT NULL,
-      description TEXT
-    );`)
-    .then(loadBooks)
+      INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING`,
+    [title, author, isbn, image_url, description]
+  )
+    .then(res.sendStatus(201))
     .catch(console.error);
-}
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
